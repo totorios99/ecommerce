7846ca39aa1b7e899be8abaf594682fd2806efb8
Antonio Rios <joseantonio_ra@live.com>
2022-06-22 21:40:45 -0500

    Add hook up Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-const Home = () => (
-    // Empty react fragment
+const Home = ({ products, bannerData }) => (
     <div>
-      
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -13,11 +12,22 @@ const Home = () => (
       </div>
 
       <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
     
       <FooterBanner />
     </div>
 );
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
 export default Home;
