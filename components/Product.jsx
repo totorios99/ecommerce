381834cb7a1e6cb4355eaf34e05066be7347f3f5
Antonio Rios <joseantonio_ra@live.com>
2022-06-29 20:21:28 -0500

    Add Layout component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,10 +1,20 @@
-
 import React from 'react';
+import Link from 'next/link';
 
-const Product = () => {
+// Required to have a link for the image inside of the sanity dashboard
+import { urlFor } from '../lib/client';
 
+const Product = ({ product : {image, name, slug, price } }) => {
     return (
-      <div>Product</div>
+      <div>
+        <Link href={`/product/${slug.current}`}>
+          <div className="product-card">
+            <img src={urlFor(image && image[0])} width={250} height={250} className="product-image"/>  
+            <p className="product-name">{name}</p>
+            <p className="product-price">${price}</p>
+          </div> 
+        </Link>
+      </div>
     )
 }
 export default Product;
